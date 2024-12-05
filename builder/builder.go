a166ca973aa13982d5b3c49ecec2a3c2655ab72f
Karol Lassak <ingwar@kentik.com>
2024-12-05 23:21:19 +0100

    Another place

--- builder/builder.go
+++ builder/builder.go
@@ -400,7 +400,9 @@ func (b *Builder) convertAndBuild(ctx context.Context, target domain.Target, opt
 
 			for _, saveImage := range b.targetPhaseImages(sts) {
 				doSave := (sts.GetDoSaves() || saveImage.ForceSave)
-				shouldExport := !opt.NoOutput && opt.OnlyArtifact == nil && !(opt.OnlyFinalTargetImages && sts != mts.Final) && saveImage.DockerTag != "" && doSave
+				shouldExport := false
+				b.opt.Console.Printf("Skipping push of image %s\n", saveImage.DockerTag)
+
 				shouldPush := opt.Push && saveImage.Push && !sts.Target.IsRemote() && saveImage.DockerTag != "" && sts.GetDoPushes()
 				useCacheHint := saveImage.CacheHint && b.opt.CacheExport != ""
 				if (saveImage.SkipBuilder || !shouldPush && !shouldExport && !useCacheHint) || (!shouldPush && saveImage.HasPushDependencies) {
